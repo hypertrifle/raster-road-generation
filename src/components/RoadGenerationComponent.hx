e31b3a6e444344dac3f57af550ca737d2f0e772a
Rich Searle <ricardo.searle@gmail.com>
2016-04-29 19:38:25 +0100

    got hills working, now needs a massive refactor

--- src/components/RoadGenerationComponent.hx
+++ src/components/RoadGenerationComponent.hx
@@ -35,7 +35,7 @@ class RoadGenerationComponent extends Component {
     	height = Luxe.screen.h;
 
         //called when initialising the component
-        resetGame();  //initRoad();
+        resetGame();
 
     }
 
@@ -76,18 +76,18 @@ class RoadGenerationComponent extends Component {
 			solid : true,
 			color: color,
 			points : [
-			new Vector(0, 0),
-			new Vector(0, 0),
-			new Vector(0, 0),
-			new Vector(0, 0)
+				new Vector(0, 0),
+				new Vector(0, 0),
+				new Vector(0, 0),
+				new Vector(0, 0)
 			],
 			visible: false
-			});
+		});
 	}
 
 	function project(p:SegmentPoint, cameraX:Float, cameraY:Float, cameraZ:Float, cameraDepth:Float, width:Float, height:Float, roadWidth:Float) {
-		p.camera.x     = 0 - cameraX;
-		p.camera.y     = 0 - cameraY;
+		p.camera.x     = p.world.x - cameraX;
+		p.camera.y     = p.world.y - cameraY;
 		p.camera.z     = p.world.z - cameraZ;
 		p.screen.scale = cameraDepth/p.camera.z;
 		p.screen.x     = Math.round((width/2)  + (p.screen.scale * p.camera.x  * width/2));
@@ -109,12 +109,12 @@ class RoadGenerationComponent extends Component {
     	return Math.floor(result);
     }
 
+
+    function accelerate(v:Float, accel:Float, dt:Float):Float      { return v + (accel * dt);}
+    function interpolate(a:Float,b:Float,percent:Float):Float       { return a + (b-a)*percent;}
+
+
     override function update(dt:Float) {
-
-    	//speed += 10;
-
-        //called every frame for you
-        //position = Util.increase(position, dt * speed, trackLength);
 
         //clear visible segments
         for(n in 0...geometries.length){
@@ -128,8 +128,18 @@ class RoadGenerationComponent extends Component {
 
         var baseSegment = findSegment(position);
         var basePercent = percentRemaining(position, Math.floor(segmentLength));
+        var playerSegment = findSegment(Math.floor(position+playerZ));
+        var playerPercent = percentRemaining(position+playerZ, segmentLength);
+        var playerY       = interpolate(playerSegment.p1.world.y, playerSegment.p2.world.y, playerPercent);
+        var maxy          = height;
+
+
         var dx = - (baseSegment.curve * basePercent);
         var x  = 0;
+
+
+
+
 
         var maxy = height;
         var segment:Segment;
@@ -137,36 +147,33 @@ class RoadGenerationComponent extends Component {
         var updatedSegments:Int =0;
 
         for(n in 0...drawDistance) {
+
         	segment = segments[(baseSegment.index + n) % segments.length];
 
-
+        	
         	segment.looped = (segment.index < baseSegment.index)? true : false;
-               segment.fog    = fog(n/drawDistance, fogDensity);
+			segment.fog    = fog(n/drawDistance, fogDensity);
 
-               segment.p1 = project(segment.p1, (playerX * roadWidth) - x, cameraHeight, position - (segment.looped ? trackLength : 0), cameraDepth, width, height, roadWidth);
-               segment.p2 = project(segment.p2, (playerX * roadWidth) - x - dx, cameraHeight, position - (segment.looped ? trackLength : 0), cameraDepth, width, height, roadWidth);
-               
-               x  += Math.floor(dx);
-               dx += segment.curve;
+			segment.p1 = project(segment.p1, (playerX * roadWidth) - x, playerY + cameraHeight, position - (segment.looped ? trackLength : 0), cameraDepth, width, height, roadWidth);
+			segment.p2 = project(segment.p2, (playerX * roadWidth) - x - dx, playerY + cameraHeight, position - (segment.looped ? trackLength : 0), cameraDepth, width, height, roadWidth);
+
+			x  += Math.floor(dx);
+			dx += segment.curve;
 
 
-               if ((segment.p1.camera.z <= cameraDepth) || (segment.p2.screen.y >= maxy)) {
-                    continue; // clip by (already rendered) segment
-                    return;
-                }   
+			if ((segment.p1.camera.z <= cameraDepth)         || // behind us
+            (segment.p2.screen.y >= segment.p1.screen.y) || // back face cull
+            (segment.p2.screen.y >= maxy)) {
+			    continue; // clip by (already rendered) segment
+			    return;
+			}   
 
-                updatedSegments ++;
-                renderSegment(segment,width,n);
-                maxy = Math.floor(segment.p2.screen.y);
+			updatedSegments ++;
+			renderSegment(segment,width,n);
+			maxy = Math.floor(segment.p2.screen.y);
 
 
             }
-
-        //clearRender();
-
-        //movePoints();
-
-        //trace("render complete: "+ updatedSegments);
     }
 
     function fog(distance:Float, density:Float) { 
@@ -184,8 +191,8 @@ class RoadGenerationComponent extends Component {
     	geomerty.backgroundPoly.visible = true;
 
 
-    	geomerty.roadPoly.color = (Math.floor(segmentIn.index/rumbleLength)%2 == 0) ? new Color().rgb(0xff4b03) : new Color().rgb(0xee4b03);
-    	geomerty.backgroundPoly.color = (Math.floor(segmentIn.index/rumbleLength)%2 == 0) ? new Color().rgb(0x21a01b) : new Color().rgb(0x21801b);
+    	geomerty.roadPoly.color = (Math.floor(segmentIn.index/rumbleLength)%2 == 0) ? new Color().rgb(0x0f5848) : new Color().rgb(0x0f4838);
+    	geomerty.backgroundPoly.color = (Math.floor(segmentIn.index/rumbleLength)%2 == 0) ? new Color().rgb(0x00da76) : new Color().rgb(0x00ca66);
 
     	geomerty.roadPoly.color.a = segmentIn.fog;
     	geomerty.backgroundPoly.color.a = segmentIn.fog;
@@ -236,23 +243,31 @@ class RoadGenerationComponent extends Component {
     	Luxe.renderer.clear(new Color().rgb(0x008800));
     }
 
+    function randomInt(max:Int):Int{
+    	return Math.floor(Math.random()*max);
+    }
+
 
     function initRoad() {
     	segments = new Array<Segment>();
 
-    	//for(var n = 0 ; n < 500 ; n++) { // arbitrary road length
-    		addStraight(Math.floor(ROAD_LENGTH_SHORT/4));
+    		for(i in 0...100){ 
+    			addRoad(randomInt(100),randomInt(100), randomInt(100), randomInt(20)-10, randomInt(150)-75);
+    		}
+
+
+    		/*addLowRollingHills(ROAD_LENGTH_SHORT, ROAD_HILL_MEDIUM);
     		addSCurves();
-    		addStraight(ROAD_LENGTH_LONG);
+    		addLowRollingHills(ROAD_LENGTH_SHORT, ROAD_HILL_MEDIUM);
     		addCurve(ROAD_LENGTH_MEDIUM, ROAD_CURVE_MEDIUM);
-    		addCurve(ROAD_LENGTH_LONG, ROAD_CURVE_MEDIUM);
+    		addLowRollingHills(ROAD_LENGTH_SHORT, ROAD_HILL_MEDIUM);
     		addStraight(ROAD_LENGTH_MEDIUM);
     		addSCurves();
     		addCurve(ROAD_LENGTH_LONG, -ROAD_CURVE_MEDIUM);
-    		addCurve(ROAD_LENGTH_LONG, ROAD_CURVE_MEDIUM);
+    		addLowRollingHills(ROAD_LENGTH_SHORT, ROAD_HILL_MEDIUM);
     		addStraight(ROAD_LENGTH_MEDIUM);
     		addSCurves();
-    		addCurve(ROAD_LENGTH_LONG, -ROAD_CURVE_EASY);
+    		addCurve(ROAD_LENGTH_LONG, -ROAD_CURVE_EASY);*/
 
 
     		trackLength = segments.length * segmentLength;
@@ -260,54 +275,79 @@ class RoadGenerationComponent extends Component {
 
     	}
 
-    	function addSegment(curve:Float) {
+    	function lastY() {
+    	  return (segments.length == 0) ? 0 : segments[segments.length-1].p2.world.y;
+    	}
+
+    	function addSegment(curve:Float,y:Float) {
     		var n = segments.length;
     		segments.push({
     			looped: false,
     			fog: 1,
     			curve: curve,
 	  	       index: n, //use this with the segments depth.
-	             p1: {world:{x:0,y:0,z:n*segmentLength,w:0,scale:0},camera:{x:0,y:0,z:0,w:0,scale:0},screen:{x:0,y:0,z:0,w:0,scale:0}}, //n   *segmentLength,
-	  	       p2: {world:{x:0,y:0,z:(n+1)*segmentLength,w:0,scale:0},camera:{x:0,y:0,z:0,w:0,scale:0},screen:{x:0,y:0,z:0,w:0,scale:0}}, //n   *segmentLength,
+	             p1: {world:{x:0,y:lastY(),z:n*segmentLength,w:0,scale:0},camera:{x:0,y:0,z:0,w:0,scale:0},screen:{x:0,y:0,z:0,w:0,scale:0}}, //n   *segmentLength,
+	  	       p2: {world:{x:0,y:y,z:(n+1)*segmentLength,w:0,scale:0},camera:{x:0,y:0,z:0,w:0,scale:0},screen:{x:0,y:0,z:0,w:0,scale:0}}, //n   *segmentLength,
 	  	       //backgroundPoly: initPoly(n,1),
 	  	       //roadPoly: initPoly(n,0)
 
 	  	       });
     	}
 
-    	function addRoad(enter:Int, hold:Int, leave:Int, curve:Int) {
+    	function addRoad(enter:Int, hold:Int, leave:Int, curve:Int, y:Int) {
+    		var startY   = lastY();
+    		var endY     = startY + (y * segmentLength);
+
     		var n;
+    		var total = enter + hold + leave;
     		for(n in 0...enter)
-    		addSegment(easeIn(0, curve, n/enter));
+    		addSegment(easeIn(0, curve, n/enter), easeInOut(Math.floor(startY), Math.floor(endY), n/total));
     		for(n in 0...hold)
-    		addSegment(curve);
+    		addSegment(curve, easeInOut(startY, endY, (enter+n)/total));
     		for(n in 0...leave)
-    		addSegment(easeInOut(curve, 0, Math.floor(n/leave)));
+    		addSegment(easeInOut(curve, 0, Math.floor(n/leave)),  easeInOut(startY, endY, (enter+hold+n)/total));
     	}
+
+    	function easeIn(a:Float,b:Float,percent:Float):Float{ return a + (b-a)*Math.pow(percent,2);                           }
+    	function easeOut(a:Float,b:Float,percent:Float):Float{ return a + (b-a)*(1-Math.pow(1-percent,2));                     }
+    	function easeInOut(a:Float,b:Float,percent:Float):Float{ return a + (b-a)*((-Math.cos(percent*Math.PI)/2) + 0.5);        }
+    	function percentRemaining(n:Float, total:Float):Float          { return (n%total)/total;                           }
+
 
 
     	function addStraight(num) {
-    		addRoad(num, num, num, 0);
+    		addRoad(num, num, num, 0,0);
     	}
 
     	function addCurve(num, curve) {
-    		addRoad(num, num, num, curve);
+    		addRoad(num, num, num, curve,0);
     	}
+
+    	function addHill(num, height) {
+    	      addRoad(num, num, num, 0, height);
+	    }
+
+	    function addLowRollingHills(num, height) {
+	          addRoad(num, num, num,  0,  Math.floor(height/2));
+	          addRoad(num, num, num,  0, -height);
+	          addRoad(num, num, num,  0,  height);
+	          addRoad(num, num, num,  0,  0);
+	          addRoad(num, num, num,  0,  Math.floor(height/2));
+	          addRoad(num, num, num,  0,  0);
+        }
+
+
 
     	function addSCurves() {
-    		addRoad(ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,  -ROAD_CURVE_EASY);
-    		addRoad(ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,   ROAD_CURVE_MEDIUM);
-    		addRoad(ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,   ROAD_CURVE_EASY);
-    		addRoad(ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,  -ROAD_CURVE_EASY);
-    		addRoad(ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,  -ROAD_CURVE_MEDIUM);
+    		addRoad(ROAD_LENGTH_SHORT, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,  -ROAD_CURVE_EASY,0);
+    		addRoad(ROAD_LENGTH_SHORT, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,   ROAD_CURVE_MEDIUM,0);
+    		addRoad(ROAD_LENGTH_SHORT, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,   ROAD_CURVE_EASY,0);
+    		addRoad(ROAD_LENGTH_SHORT, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,  -ROAD_CURVE_EASY,0);
+    		addRoad(ROAD_LENGTH_SHORT, ROAD_LENGTH_MEDIUM, ROAD_LENGTH_MEDIUM,  -ROAD_CURVE_MEDIUM,0);
     	}
 
 
 
-    	function easeIn(a,b,percent){ return a + (b-a)*Math.pow(percent,2);                           }
-    	function easeOut(a,b,percent){ return a + (b-a)*(1-Math.pow(1-percent,2));                     }
-    	function easeInOut(a,b,percent){ return a + (b-a)*((-Math.cos(percent*Math.PI)/2) + 0.5);        }
-    	function percentRemaining(n, total)          { return (n%total)/total;                           }
     	public static inline var ROAD_LENGTH_NONE:Float = 0;
     	public static inline var ROAD_LENGTH_SHORT:Float = 25;
     	public static inline var ROAD_LENGTH_MEDIUM:Float = 50;
@@ -317,6 +357,16 @@ class RoadGenerationComponent extends Component {
     	public static inline var ROAD_CURVE_EASY:Float = 2;
     	public static inline var ROAD_CURVE_MEDIUM:Float = 4;
     	public static inline var ROAD_CURVE_HARD:Float = 6;
+
+
+    	public static inline var ROAD_HILL_NONE:Float = 0;
+    	public static inline var ROAD_HILL_LOW:Float = 20;
+    	public static inline var ROAD_HILL_MEDIUM:Float = 40;
+    	public static inline var ROAD_HILL_HIGH:Float = 60;
+
+    	/*
+    	HILL:   { NONE: 0, LOW:    20, MEDIUM:  40, HIGH:   60 },
+    	*/
 
     }
 
